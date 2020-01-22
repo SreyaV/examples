56cdcfc2e43e309a2790a8076635b2e8f0ea4dfe
Ubuntu <svangara@sreyatest.ghyxinb4nynebfilokeu0ezsme.cx.internal.cloudapp.net>
2020-01-22 20:56:51 +0000

    added pipeline2 tar to test

--- pipelines/azurepipeline/code/training/train.py
+++ pipelines/azurepipeline/code/training/train.py
@@ -8,8 +8,7 @@ import argparse
 from random import shuffle
 from pathlib2 import Path
 import numpy as np
-# import tensorflow as tf
-# from tensorflow.data import Dataset
+import sys
 
 
 def info(msg, char="#", width=75):
@@ -23,15 +22,6 @@ def check_dir(path):
   if not os.path.exists(path):
     os.makedirs(path)
   return Path(path).resolve(strict=False)
-
-
-def process_image(path, label, img_size):
-  return None
-  # img_raw = tf.io.read_file(path)
-  # img_tensor = tf.image.decode_jpeg(img_raw, channels=3)
-  # img_final = tf.image.resize(img_tensor, [img_size, img_size]) / 255
-  # return img_final, label
-
 
 def load_dataset(base_path, dset, split=None):
   # normalize splits
@@ -64,78 +54,6 @@ def load_dataset(base_path, dset, split=None):
 
   return data[:train_idx]
 
-
-# @print_info
-def run(
-    dpath,
-    img_size=160,
-    epochs=10,
-    batch_size=32,
-    learning_rate=0.0001,
-    output='model',
-    dset=None):
-  img_shape = (img_size, img_size, 3)
-
-  info('Loading Data Set')
-  # load dataset
-  train = load_dataset(dpath, dset)
-
-  # training data
-  train_data, train_labels = zip(*train)
-  # train_ds = Dataset.zip((Dataset.from_tensor_slices(list(train_data)),
-  #                         Dataset.from_tensor_slices(list(train_labels)), img_size))
-
-  train_ds = train_ds.map(map_func=process_image,
-                          num_parallel_calls=5)
-
-  # train_ds = train_ds.apply(tf.data.experimental.ignore_errors())
-
-  train_ds = train_ds.batch(batch_size)
-  train_ds = train_ds.prefetch(buffer_size=5)
-  train_ds = train_ds.repeat()
-
-  # model
-  info('Creating Model')
-  # base_model = tf.keras.applications.MobileNetV2(input_shape=img_shape,
-  #                                                include_top=False,
-  #                                                weights='imagenet')
-  # base_model.trainable = True
-
-  # model = tf.keras.Sequential([
-  #   base_model,
-  #   tf.keras.layers.GlobalAveragePooling2D(),
-  #   tf.keras.layers.Dense(1, activation='sigmoid')
-  # ])
-
-  # model.compile(optimizer=tf.keras.optimizers.Adam(lr=learning_rate),
-  #               loss='binary_crossentropy',
-  #               metrics=['accuracy'])
-
-  # model.summary()
-
-  # training
-  info('Training')
-  steps_per_epoch = math.ceil(len(train) / batch_size)
-  # model.fit(train_ds, epochs=epochs, steps_per_epoch=steps_per_epoch)
-
-  # # save model
-  # info('Saving Model')
-
-  # # check existence of base model folder
-  # output = check_dir(output)
-
-  # print('Serializing into saved_model format')
-  # tf.saved_model.save(model, str(output))
-  print('Done!')
-
-  # add time prefix folder
-  file_output = str(Path(output).joinpath('latest.h5'))
-  print('Serializing h5 model to:\n{}'.format(file_output))
-  # model.save(file_output)
-
-  return generate_hash(file_output, 'kf_pipeline')
-
-
 def generate_hash(dfile, key):
   print('Generating hash for {}'.format(dfile))
   m = hmac.new(str.encode(key), digestmod=hashlib.sha256)
@@ -153,7 +71,8 @@ def generate_hash(dfile, key):
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description='transfer learning for binary image task')
   parser.add_argument('-s', '--base_path', help='directory to base data', default='../../data')
-  parser.add_argument('-d', '--data', help='directory to training and test data', default='train')
+  parser.add_argument('-d', '--data', help='directory to training and test data', default='trainInput.txt')
+  parser.add_argument('-y', '--data2', help='target file to hold output data', default='trainOutput.txt')
   parser.add_argument('-e', '--epochs', help='number of epochs', default=10, type=int)
   parser.add_argument('-b', '--batch', help='batch size', default=32, type=int)
   parser.add_argument('-i', '--image_size', help='image size', default=160, type=int)
@@ -162,39 +81,75 @@ if __name__ == "__main__":
   parser.add_argument('-f', '--dataset', help='cleaned data listing')
   args = parser.parse_args()
 
-  # info('Using TensorFlow v.{}'.format(tf.__version__))
-
   data_path = Path(args.base_path).joinpath(args.data).resolve(strict=False)
+  data_output_path = Path(args.base_path).joinpath(args.data2).resolve(strict=False)
   target_path = Path(args.base_path).resolve(strict=False).joinpath(args.outputs)
-  dataset = Path(args.base_path).joinpath(args.dataset)
-  image_size = args.image_size
+  
+  print("data path " + str(data_path) + "\n")
+  print("data output " + str(data_output_path) + "\n")
+  print("target path " + str(target_path) + "\n")
+  # dataset = Path(args.base_path).joinpath(args.dataset)
+  # print("full data path ", str(dataset) + "\n")
+  # image_size = args.image_size
 
-  params = Path(args.base_path).joinpath('params.json')
 
-  args = {
-    "dpath": str(data_path),
-    "img_size": image_size,
-    "epochs": args.epochs,
-    "batch_size": args.batch,
-    "learning_rate": args.lr,
-    "output": str(target_path),
-    "dset": str(dataset)
-  }
+  # params = Path(args.base_path).joinpath('params.json')
 
-  dataset_signature = generate_hash(dataset, 'kf_pipeline')
-  # printing out args for posterity
-  for i in args:
-    print('{} => {}'.format(i, args[i]))
+  # args = {
+  #   "dpath": str(data_path),
+  #   "img_size": image_size,
+  #   "epochs": args.epochs,
+  #   "batch_size": args.batch,
+  #   "learning_rate": args.lr,
+  #   "output": str(target_path),
+  #   "dset": str(dataset)
+  # }
 
-  model_signature = run(**args)
+  TRAIN_INPUT = np.loadtxt(str(data_path))
+  print(TRAIN_INPUT)
+  print('\n' + str(np.shape(TRAIN_INPUT)))
 
-  args['dataset_signature'] = dataset_signature.upper()
-  args['model_signature'] = model_signature.upper()
-  args['model_type'] = 'tfv2-MobileNetV2'
-  print('Writing out params...', end='')
-  with open(str(params), 'w') as f:
-    json.dump(args, f)
+  TRAIN_OUTPUT = np.loadtxt(str(data_output_path))
+  print(TRAIN_OUTPUT)
+  print('\n' + str(np.shape(TRAIN_OUTPUT)))
 
-  print(' Saved to {}'.format(str(params)))
+  # TRAIN_SET_COUNT = 100
+  # TRAIN_INPUT= list()
+  # TRAIN_OUTPUT = list()
+
+  # with open(str(data_path), 'r') as filehandle:
+  #   i = 0
+  #   for line in filehandle:
+  #     currentPlace = line[:-2]
+  #     currentPlace = currentPlace[1:]
+  #     if i < TRAIN_SET_COUNT:
+  #       currentPlace = list(currentPlace.split(','))
+  #       TRAIN_INPUT.append(currentPlace)
+  #     else:
+  #       # currentPlace = list(currentPlace)
+  #       TRAIN_OUTPUT.append(currentPlace)
+  #     i += 1
+    
+  from sklearn.linear_model import LinearRegression
+
+  predictor = LinearRegression(n_jobs=-1)
+  predictor.fit(X=TRAIN_INPUT, y=TRAIN_OUTPUT)
+  coefficients = predictor.coef_
+
+  # dataset_signature = generate_hash(dataset, 'kf_pipeline')
+  # # printing out args for posterity
+  # for i in args:
+  #   print('{} => {}'.format(i, args[i]))
+
+  # model_signature = run(**args)
+
+  # args['dataset_signature'] = dataset_signature.upper()
+  # args['model_signature'] = model_signature.upper()
+  # args['model_type'] = 'tfv2-MobileNetV2'
+  # print('Writing out params...', end='')
+  # with open(str(params), 'w') as f:
+  #   json.dump(args, f)
+
+  print(' Saved to {}'.format(str(predictor)))
 
   # python train.py -d train -e 3 -b 32 -l 0.0001 -o model -f train.txt
